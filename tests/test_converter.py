bb98ee5fcd5730db20e8ecff2728309720996a97
Bruno Riwerson Silva <riwerson@gmail.com>
2023-02-20 08:10:18 -0300

    test: use cases for int_to_roman_numeral function

---
+++ tests/test_converter.py
@@ -0,0 +1,10 @@
+import pytest
+from src.converter import int_to_roman_numeral
+
+
+def test_int_to_roman_numeral():
+    assert int_to_roman_numeral(19) == "XIX"
+    assert int_to_roman_numeral(1989) == "MCMLXXIX"
+    assert int_to_roman_numeral(3999) == "MMMCMXCIX"
+    assert int_to_roman_numeral("19") == "O número que será convertido precisa ser um inteiro."
+    assert int_to_roman_numeral(4000) == "O número que será convertido não pode ser maior do que 3999."
